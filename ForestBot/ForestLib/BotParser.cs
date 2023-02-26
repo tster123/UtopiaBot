8f5b196e57c3e09fa2739e0adb30fd37da855738
tster123 <thboone@gmail.com>
2023-02-26 15:25:06 -0800

    ?

---
+++ ForestBot/ForestLib/BotParser.cs
@@ -0,0 +1,34 @@
+﻿namespace ForestLib
+{
+
+    public enum ActionType
+    {
+        Attack,
+        Spell,
+        Thievery
+    }
+    public class Action
+    {
+        public ActionType Type;
+        public string OpName;
+        public bool Success;
+        public int Damage;
+        public int ThievesLost;
+        public int WizardsLost;
+        public int HorsesLost;
+        public int OffSpecsLost;
+        public int ElitesLost;
+        public int AttackKills;
+        public int PrisonersGained;
+        public int PeasantsGained;
+        public double ReturnHours;
+        public int SpecCredits;
+        public int BuildCredits;
+        public int Promotions;
+        public int OffenseSent;
+        public int GeneralsSent;
+    }
+    public class BotParser
+    {
+    }
+}
---
+++ ForestBot/ForestTests/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿namespace ForestTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        string ops = @"
+☄️💚 Gamegear gamegea#  <<fireball | 99 German NoNo 99 (5:12)>> 310|17% guilds (72% BE|1.02 (m.1.55)) vs 0.72 (m.0.76)|rNW 0.93| total 6107 (0.05)
+
+☄️💚 B A Start b a star#  <<drought | 99 n00bs (5:12)>> 16|19.7% guilds (93% BE|0.87 (m.1.11))|rNW 0.84
+
+
+🌟💔 WTF is a gamecube wtf is a gamecub#  <<minor protection>> FAIL | 10.5% guilds (74% BE (m.7.8))
+🌟💚 PS Vita ps vit#  <<magic shield>> 19 | 18.2% guilds (62% BE (m.11.3))
+🌟💚 Gamegear gamegea#  <<ghost workers>> 14 | 17% guilds (70% BE (m.11.9))
+🌟💚 AllYourBaseAreBelongToUs allyourbasearebelongtou#  <<magic shield>> 13 | 9.9% guilds (60% BE (m.5.9))
+
+🕵️💚 PS Vita ps vit#  <<spy on military | 99 Road Apples (5:12)>>|217 sent (0.46)|4.63 (m.5.75)|rNW 1.01
+🕵️💔 Duck Hunt duck hun#  <<survey | 99 problems (5:12)>> FAIL (-9 thieves)|190 sent (0.31)|3.17 (m.3.72) vs 2.5 (m.2.65)|rNW 0.92
+
+
+🕵️💚 Gamegear gamegea#  <<propaganda elites | 99 n00bs (5:12)>> 14|3570 sent (6.41)|6.42 (m.14.71) vs 0.66 (m.0.67)|rNW 1.02
+🕵️💚 Gamegear gamegea#  <<propaganda thieves | 99 n00bs (5:12)>> 29|3570 sent (6.41)|6.42 (m.14.71) vs 0.66 (m.0.67)|rNW 1.02
+🕵️💔 B A Start b a star#  <<rob the granaries | 99 n00bs (5:12)>> FAIL (- thieves)|25 sent (0.04)|3.33 (m.3.85) vs 0.7 (m.0.71)|rNW 0.84
+🕵️💚  up up down down Up Up Down Down <<night strike | 99 n00bs (5:12)>> 8|214 sent (0.28)|2.8 (m.3.21) vs 0.62 (m.0.63)|rNW 1.27
+🕵️💔 B A Start b a star#  <<rob the granaries | 99 n00bs (5:12)>> FAIL (- thieves)|25 sent (0.04)|3.33 (m.3.85) vs 0.7 (m.0.71)|rNW 0.84
+
+
+:crossed_swords: left right left right [left right left righ#] attacked 99 Red Balloons (5:12)|captured: 66|loss: 89 Swordsmen, 62 Knights and 151 horses|kills: 110 (+64 prisoners)|return: 13.09|217 spec creds|218 OS promoted|58536off (1 gens)
+:crossed_swords: I ate my kids candy [i ate my kids cand#] attacked Ultra Super Knight of Erin (5:9)|captured: 61|loss: 33 Griffins|kills: 19 (+35 prisoners)|return: 8.93|47 spec creds|118 OS promoted|141 peasants|13903off (1 gens)
+:crossed_swords: Licks banana at store [licks banana at stor#] attacked Barium Buttocks (4:8)|learn: 186,964 |loss: 88 Goblins, 520 Ogres and 609 horses|kills: 534 (+150 prisoners)|return: 16.26|261620off (1 gens)
+:crossed_swords: gave trick-or-treaters ex-lax [gave trick-or-treaters ex-la#] attacked No White Flag (6:2)|razed: 129|loss: 225 Night Rangers and 225 horses|kills: 71 (+131 prisoners)|return: 10.77|60395off (1 gens)
+:crossed_swords: I let kingy think I care [i let kingy think i car#] attacked Fury Phoenix (3:2)|recaptured: 105|loss: 399 Rangers, 85 Elf Lords and 461 horses|kills: 632|return: 12.00|84742off (1 gens)
+:crossed_swords: Called out on first day [called out on first da#] attacked Pandemonium Knight of Erin (5:9)|killed: 777 peasants, thieves, and wizards|loss: 84 Night Rangers, 12 Drows and 96 horses|kills: 12 (+27 prisoners)|return: 11.90|22357off (2 gens)
+";
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+
+   
+}
