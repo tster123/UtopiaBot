e1576987858f7d1c3bde2a1030f8db9aefd76fe3
Tyler Boone <thboone@gmail.com>
2023-03-05 23:39:31 -0800

    more work on ticket

---
+++ ForestBot/ForestLib/Tools/HowManyArmouries.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForestLib.Tools
+{
+    public class HowManyArmouries
+    {
+        public int NumSoldiersToTrain { get; set; }
+        public int TargetThieves { get; set; }
+        public int AvailableGc { get; set; }
+        public int BuildCredits { get; set; }
+        public int BuildCost { get; set; }
+        public int RazeCost { get; set; }
+        public int Acres { get; set; }
+    }
+}
