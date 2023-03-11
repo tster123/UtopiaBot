55e2e5aa6d6eec8f15489f1711c9ba7eef5cefdf
tster123 <thboone@gmail.com>
2023-03-11 07:04:34 -0800

    updating timeline

--- ForestBot/ForestLib/StrategySettings.cs
+++ ForestBot/ForestLib/StrategySettings.cs
@@ -4,17 +4,17 @@
     {
         public double TargetHorseFill = 0.9;
 
-        public double ExpectedExpedientStrength = 1.0,
-            AverageConstructionScience = 0.03,
-            AverageTrainingTimeScience = 0.04,
-            ProductionScience = .30,
-            ToolsScience = .05;
+        public double ExpectedExpedientStrength = 1.1,
+            AverageConstructionScience = 0.04,
+            AverageTrainingTimeScience = 0.06,
+            ProductionScience = .47,
+            ToolsScience = .12;
 
         public double RitualSuccessRate = 0.80;
         public double RitualCastsDesired = 5;
         public double RitualBuffer = .2;
 
-        public double    AveragePeonsPerAcre = 15,
+        public double    AveragePeonsPerAcre = 16,
             TargetPeonsPerAcre = 6;
 
     }
--- ForestBot/ForestTests/CeasefireTimelineTest.cs
+++ ForestBot/ForestTests/CeasefireTimelineTest.cs
@@ -10,7 +10,7 @@ namespace ForestTests
         public void TestTimeline()
         {
             CeasefireTimeline timeline = new CeasefireTimeline(new Age100Settings(), new StrategySettings());
-            var ret = timeline.GetTimeline(new UtopiaDate(3, 1, 1));
+            var ret = timeline.GetTimeline(new UtopiaDate(3, 7, 21));
             foreach (var e in ret)
             {
                 Console.WriteLine(e);
