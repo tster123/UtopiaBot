981c991793c9a1f134d2754a922f3c6e8c4d3eb5
Tyler Boone <thboone@gmail.com>
2024-02-24 11:21:05 -0800

    updating dependencies

---
+++ ForestBot/ForestBot/IntelEndpoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForestBot
+{
+    public class IntelEndpoint
+    {
+        public void StartEndpoint(int port)
+        {
+            //var builder = WebApplication
+        }
+    }
+}
